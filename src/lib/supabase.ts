7329699c463c7b96f4abb5a62f4d072defa12752
bkwinstin <brian@justicesystempartners.org>
2026-05-26 10:10:50 -0400

    Fix Supabase URL & key env var usage

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// This explicitly points to your unique database instead of the main website
-const supabaseUrl = 'https://supabase.co';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
