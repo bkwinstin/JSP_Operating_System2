c8d5d7136536d0aa53f6e2a0481f5b93f5d67b82
bkwinstin <brian@justicesystempartners.org>
2026-05-26 09:49:18 -0400

    Ask for user changes

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+// Hardcoding the URL directly fixes Netlify's local pathing bugs
+const supabaseUrl = 'https://supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
